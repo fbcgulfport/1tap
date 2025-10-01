23fb1fdb37a0ffe218ffa1c9baf053c9fe253d68
Will Shadow <wshadow@fbcgulfport.org>
2025-10-01 11:48:33 -0500

    fix build

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,5 @@
 import { drizzle } from "drizzle-orm/bun-sqlite"
 
-const dbUrl =
-	process.env.NODE_ENV === "production" ? "/app/data/app.db" : "./local.db"
+const dbUrl = "./local.db"
+// process.env.NODE_ENV === "production" ? "/app/data/app.db" : "./local.db"
 export const db = drizzle(dbUrl)
