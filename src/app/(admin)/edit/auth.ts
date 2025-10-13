6f550680a2f0623440ea558d6b02a82d64944bdb
Will Shadow <wshadow@fbcgulfport.org>
2025-10-13 11:16:44 -0500

    remove old

--- src/app/(admin)/edit/auth.ts
+++
@@ -1,8 +0,0 @@
-"use server"
-
-import { env } from "~/lib/env"
-
-export async function verifyPassword(password: string): Promise<boolean> {
-	// Simple password check against environment variable
-	return password === env.EDIT_PASSWORD
-}
